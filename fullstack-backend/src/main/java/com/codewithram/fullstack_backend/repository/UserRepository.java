d57599907fdff74238f9481102d970393300f5f3
ramanjineyulugosi <gosiramanjineyulu868@gmail.com>
2024-09-21 19:53:56 +0530

    Add files via upload

---
+++ fullstack-backend/src/main/java/com/codewithram/fullstack_backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codewithram.fullstack_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.codewithram.fullstack_backend.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
